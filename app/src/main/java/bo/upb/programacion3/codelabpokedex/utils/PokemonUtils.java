34eb7a99fa3ce640d50bc26987504b0b5ef5b996
Manu200162 <manli200134@gmail.com>
2020-06-21 23:07:48 -0400

    pokemons_added

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -27,6 +27,17 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(13, "Aron",R.drawable.aron,"Steel/Rock" ));
+        pokemons.add(new Pokemon(14,"Golem",R.drawable.golem,"Ground/Rock" ));
+        pokemons.add(new Pokemon(15, "Hypno",R.drawable.hypno,"Psychic"));
+        pokemons.add(new Pokemon(16, "Machoke",R.drawable.machoke, "Fighting"));
+        pokemons.add(new Pokemon(17, "Magmar",R.drawable.magmar,"Fire"));
+        pokemons.add(new Pokemon(18,"Nidoking",R.drawable.nidoking,"Poison/Ground"));
+        pokemons.add(new Pokemon(19, "Onix",R.drawable.onix,"Ground/Rock"));
+        pokemons.add(new Pokemon(20,"Rapidash",R.drawable.rapidash,"Fire"));
+        pokemons.add(new Pokemon(21,"Rhydon",R.drawable.rhydon,"Ground/Rock"));
+        pokemons.add(new Pokemon(22,"Scyther",R.drawable.scyther,"Bug/Flying"));
+
 
         return pokemons;
     }
